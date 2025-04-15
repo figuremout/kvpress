21273f564e1f6423cef50da8c2880862a880e7bc
figuremout <1344584929@qq.com>
2025-04-15 16:24:44 +0800

    Fix: Reduce KV Cache Size Error and add tests
    Signed-off-by: figuremout <1344584929@qq.com>

--- kvpress/presses/pyramidkv_press.py
+++ kvpress/presses/pyramidkv_press.py
@@ -21,6 +21,16 @@ class PyramidKVPress(SnapKVPress):
 
     We use the budget calculation formula from:
     https://github.com/Zefan-Cai/KVCache-Factory/blob/main/pyramidkv/pyramidkv_utils.py#L197
+
+    This implementation always applies compression_ratio,
+    instead of disabling compression or keeping fixed budget for short queries like the original code.
+
+    max_capacity_prompt is calculated as:
+    max_num + min_num   &= (max_capacity_prompt - window_size) * 2
+    total_kvcache_size  &= \frac{(max_num + min_num) * num_layers}{2}
+                        &= (max_capacity_prompt - window_size) * num_layers
+    total_kvcache_size  &= query_length * num_layers * (1 - compression_ratio)
+    max_capacity_prompt &= window_size + query_length * (1 - compression_ratio)
     """
 
     compression_ratio: float = 0.0
@@ -39,9 +49,9 @@ class PyramidKVPress(SnapKVPress):
         assert self.beta >= 1, "Beta should >= 1"
 
         # Ensure the total budget meets the compression_ratio requirements
-        max_capacity_prompt = self.window_size + int(q_len * (1 - self.compression_ratio))
+        max_capacity_prompt = self.window_size + q_len * (1 - self.compression_ratio)
 
-        min_num = (max_capacity_prompt - self.window_size) // self.beta
+        min_num = (max_capacity_prompt - self.window_size) / self.beta
         max_num = (max_capacity_prompt - self.window_size) * 2 - min_num
 
         if max_num >= q_len - self.window_size:
@@ -50,13 +60,11 @@ class PyramidKVPress(SnapKVPress):
 
         if not (q_len >= max_num >= min_num >= self.window_size):
             # Fall back to SnapKV
-            max_num = int(q_len * (1 - self.compression_ratio))
-            min_num = max_num
+            return round(q_len * (1 - self.compression_ratio))
 
-        steps = (max_num - min_num) // (module.config.num_hidden_layers - 1)
-        return max_num - module.layer_idx * steps
+        steps = (max_num - min_num) / (module.config.num_hidden_layers - 1)
+        return round(max_num - module.layer_idx * steps)
 
-    # Overriding ScorerPress compress method
     def compress(
         self,
         module: nn.Module,
